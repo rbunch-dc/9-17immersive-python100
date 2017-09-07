253a1d47e281ecc0e8cd11fcedb3d21a50bce9de
Robert Bunch <rob@digitalcrafts.com>
2017-09-07 11:58:52 -0400

    Functinos

--- function_practice.py
+++ function_practice.py
@@ -32,4 +32,22 @@
 # for i in range(0,len(students)):
 # 	say_hello_with_name(students[i])
 
-say_hello_with_name() #THIS WILL FAIL!!
+# say_hello_with_name() #THIS WILL FAIL!!
+
+# You can set a default for a parameter
+def say_hello_safe(name, in_class="Yes"):
+	print "%s, %s is in the class" % (in_class, name)
+
+say_hello_safe("Jong")
+say_hello_safe("Max", "No")
+# say_hello_safe("Max", "No", "Not in class")
+
+# functions ALWAYS return a value
+# return value is a functions chance to send ONE and only ONE thing back to 
+# whoever called it
+def return_user_name(name):
+	normalized_name = name.upper()
+	return normalized_name
+
+print return_user_name("Chris")
+print return_user_name("Im a WiLD anD CrAZy GUy")
