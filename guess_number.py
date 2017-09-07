23a3f5cfade2537d55134db4568d465bada10c75
Robert Bunch <rob@digitalcrafts.com>
2017-09-07 09:31:27 -0400

    Add comments

--- guess_number.py
+++ guess_number.py
@@ -1,14 +1,19 @@
 import random
 
+#boolean, which will be a flag/switch
 keep_playing = True
+# Get the loop going as long as the bool is true
 while keep_playing == True:
+	# Get a random number between 1-10
 	correct_number = random.randint(1,10);
-	print correct_number
+	# print correct_number
 	# print guess
+	# Set up a bool to keep guessing as long as is true
 	keep_guessing = True
 	total_tries = 5
 	used_tries = 0;
 	while keep_guessing == True:
+		# Get a guess from users
 		guess = int(raw_input("Guess a number between 1-10\n"))
 		# guess = raw_input("Guess a number between 1-10\n")
 		# try:
@@ -17,9 +22,11 @@ while keep_playing == True:
 		# 	print "You must enter a number!"
 		# else:
 		# 	guess = convert_guess
+		# user just guessed. Count it!
 		used_tries += 1;
 		if(guess == correct_number):
 			print "You Win!"
+			# switch bool to false so the guess loop terminates
 			keep_guessing = False
 		elif(total_tries == used_tries):
 			print "Sorry. You are out of guesses! The number was %d" % correct_number
